f2af3a0ef6cfe930fe4f76dc797ac75a7741bfde
KKrow <sideegod13@gmail.com>
2023-06-11 16:40:33 +0900

    Oauth commit

--- src/test/java/com/momstouch/momstouchbe/OAuth2LoginControllerTest.java
+++ src/test/java/com/momstouch/momstouchbe/OAuth2LoginControllerTest.java
@@ -1,8 +1,12 @@
 package com.momstouch.momstouchbe;
 
+import com.momstouch.momstouchbe.domain.member.Service.CustomOAuth2UserService;
+import com.momstouch.momstouchbe.domain.member.model.Account;
 import com.momstouch.momstouchbe.domain.member.model.Member;
+import com.momstouch.momstouchbe.domain.member.model.OAuthAttributes;
 import com.momstouch.momstouchbe.domain.member.repository.MemberRepository;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,13 +20,15 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 
-
-
+import javax.servlet.http.HttpSession;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.anyString;
 
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -66,10 +72,49 @@ public class OAuth2LoginControllerTest {
                 .andExpect(status().isOk());
     }
 
-    @Test
-    void 멤버_수정테스트() throws Exception{
 
+    @Test
+    void 저장_수정_테스트() {
+        // Mock 데이터 준비
+        MemberRepository memberRepository = mock(MemberRepository.class);
+        HttpSession httpSession = mock(HttpSession.class);
+        CustomOAuth2UserService service = new CustomOAuth2UserService(memberRepository, httpSession);
+
+        // Mock memberRepository.findByEmail
+        OAuthAttributes attributes = OAuthAttributes.builder()
+                .email("test@example.com")
+                .name("Test Name")
+                .role("ROLE_TEST")
+                .build();
+        Member existingMember = Member.builder()
+                .email("test@example.com")
+                .account(Account.builder().name("Existing Member").role("ROLE_TEST").build())
+                .build();
+        when(memberRepository.findByEmail(anyString())).thenReturn(Optional.of(existingMember));
+
+        // Mock memberRepository.save
+        Member savedMember = Member.builder()
+                .email("test@example.com")
+                .account(Account.builder().name("Updated Member").role("ROLE_UPDATED").build())
+                .build();
+        when(memberRepository.save(any(Member.class))).thenReturn(savedMember);
+
+        // 테스트할 메서드 호출
+        Member result = service.saveOrUpdate(attributes);
+
+        // memberRepository.save가 호출되었는지 확인
+        verify(memberRepository, times(1)).save(any(Member.class));
+
+        // 결과 검증
+        assertNotNull(result);
+        assertEquals(savedMember, result);
     }
+
+
+
+
+
+
 
 
 
